2c1882bec7cbe582faa63f2bfb0f2cdd9d999142
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-04-12 12:21:03 +0200

    store to history
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- zsh-ai-commands.zsh
+++ zsh-ai-commands.zsh
@@ -15,12 +15,28 @@
 
 (( ! ${+ZSH_AI_COMMANDS_EXPLAINER} )) && typeset -g ZSH_AI_COMMANDS_EXPLAINER=true
 
+(( ! ${+ZSH_AI_COMMANDS_HISTORY} )) && typeset -g ZSH_AI_COMMANDS_HISTORY=false
+
 fzf_ai_commands() {
   setopt extendedglob
 
   [ -n "$BUFFER" ] || { echo "Empty prompt" ; return }
 
+  BUFFER="$(echo "$BUFFER" | sed 's/^AI_ASK: //g')"
+
   ZSH_AI_COMMANDS_USER_QUERY=$BUFFER
+
+  if [ $ZSH_AI_COMMANDS_HISTORY = true ]
+  then
+    # save to history
+    echo "AI_ASK: $ZSH_AI_COMMANDS_USER_QUERY" >> $HISTFILE
+    # also to atuin's history if installed
+    if command -v atuin &> /dev/null;
+    then
+        atuin_id=$(atuin history start "AI_ASK: $ZSH_AI_COMMANDS_USER_QUERY")
+        atuin history end --exit 0 "$atuin_id"
+    fi
+  fi
 
   # FIXME: For some reason the buffer is only updated if zsh-autosuggestions is enabled
   BUFFER="Asking $ZSH_AI_COMMANDS_LLM_NAME for a command to do: $ZSH_AI_COMMANDS_USER_QUERY. Please wait..."
